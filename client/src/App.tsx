e5cec69556054b9b4b4a747ef55ce811615099f2
nicolascallatayd <nicolas.callatayd@gmail.com>
2026-08-10 12:29:48 +0100

    ....

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,5 @@
-import { Switch, Route } from "wouter";
+import { Switch, Route, useLocation } from "wouter";
+import { useEffect } from "react";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
@@ -8,10 +9,20 @@ import Home from "@/pages/Home";
 import Admin from "@/pages/Admin";
 import Login from "@/pages/Login";
 
+function RedirectToLogin() {
+  const [, navigate] = useLocation();
+
+  useEffect(() => {
+    navigate("/login");
+  }, [navigate]);
+
+  return null;
+}
+
 function Router() {
   return (
     <Switch>
-      <Route path="/" component={Home} />
+      <Route path="/" component={RedirectToLogin} />
       <Route path="/login" component={Login} />
       <Route path="/admin" component={Admin} />
       <Route component={NotFound} />
